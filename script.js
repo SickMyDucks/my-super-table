c3142944899698533a297a5fa57451c0f27d4adc
SickMyDucks <me@neilrichter.com>
2018-02-21 18:51:41 +0100

    Prefered Flexboxes to `<div>`s with `display: table`

--- script.js
+++ script.js
@@ -1,73 +1,46 @@
-
-
-// initSuperTable({
-//     element: superTable,
-//     data: users,
-//     options: {
-//         firstColumn: 'email',
-//         rowMouseOver: true,
-//         rowStyle: 'zebra',
-//         fixedHeader: true,
-//         fixedColumn: true
-//     }
-// });
-
 window.onload = function() {
-    var superTable = document.querySelector('#super-table');
-    var users = $.ajax({
+    $.ajax({
         method: 'get',
         dataType: 'json',
         url: 'users.json',
         success: function (users) {
-            for (var i in users) {
-                getElement('#super-table').append(createRow(users[i]));
+            columns = ['email', 'firstName', 'lastName', 'birthdate'];
+            for (var i in columns) {
+                var column = CreateElement('div', 'col', '');
+                column.append(CreateElement('div', 'table-header', columns[i]));
+                column.append(CreateElement('div', 'hide', '.'));
+                for (var j in users) {
+                    column.append(CreateElement('div', '', users[j][columns[i]]));
+                }
+                document.querySelector('.container').append(column);
             }
-            return users;
         }
     });
-};
-
-/**
- * Initialize the table with given options or the default ones if none given
- * @param {object} settings 
- */
-function initSuperTable(settings) {
-    $('.row').css({"height": "30px"});
 }
 
 /**
- * document.querySelector alias
- * @param {string} selector
- * @returns {Element}
- */
-function getElement(selector) {
-    return document.querySelector(selector);
-}
-
-/**
- * Creates a row with userData at the end of given element
- * @param {Object} userData 
- * @param {Element} element 
- */
-function createRow(userData) {
-    var row = CreateElement('div', 'row', '');
-    for (var i in userData) {
-        row.append(CreateElement('div', '', userData[i]));
-    }
-    return row;
-}
-
-/**
- * Creates an element with a specific class
+ * Creates an HTML element with given class and content
  * @param {string} tag 
  * @param {string} className 
  * @param {string} content 
- * @returns {Element}
+ * @returns {object} HTML element
  */
-
 function CreateElement(tag, className, content) {
     element = document.createElement(tag);
     element.classList = className;
     element.innerHTML = content;
     return element;
+}
+
+/**
+ * Sorts columns to put the chosen one first
+ * @param {array} columns 
+ * @param {string} firstColumn 
+ * @returns {array} columns
+ */
+function sortColumnsOrder(columns, firstColumn) {
+    console.table(columns);
+    columns.splice(columns.indexOf(firstColumn), 1);
+    columns.unshift(firstColumn);
+    return columns;
 }
